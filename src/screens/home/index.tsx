7ff31ac0e296551dbb4909bb67727ed36fc2be99
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-06-19 17:35:33 +0100

    react-icons and new card style

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './style.css';
+import Card from '../../components/card';
 
 const Home = () => {
   return (
@@ -8,22 +9,10 @@ const Home = () => {
         <input type="text" placeholder='Search a project...'/>
       </form>
       <div className="projects">
-        <div className="project-card">
-          <img src="https://via.placeholder.com/192" alt=""/>
-          <div className="project-card-body">
-            <h5>Level</h5>
-            <h3>Project Name</h3>
-            <button>See more...</button>
-          </div>
-        </div>
-        <div className="project-card">
-          <img src="https://via.placeholder.com/1920x1080" alt=""/>
-          <div className="project-card-body">
-            <h5>Level</h5>
-            <h3>Project Name</h3>
-            <button>See more...</button>
-          </div>
-        </div>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </main>
   );
