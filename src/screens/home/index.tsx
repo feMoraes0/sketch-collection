cf36a1851916d2478609bf6e66a57b1004e7f7d2
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-06-19 17:01:04 +0100

    added home cards

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -7,6 +7,24 @@ const Home = () => {
       <form action='/'>
         <input type="text" placeholder='Search a project...'/>
       </form>
+      <div className="projects">
+        <div className="project-card">
+          <img src="https://via.placeholder.com/192" alt=""/>
+          <div className="project-card-body">
+            <h5>Level</h5>
+            <h3>Project Name</h3>
+            <button>See more...</button>
+          </div>
+        </div>
+        <div className="project-card">
+          <img src="https://via.placeholder.com/1920x1080" alt=""/>
+          <div className="project-card-body">
+            <h5>Level</h5>
+            <h3>Project Name</h3>
+            <button>See more...</button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
