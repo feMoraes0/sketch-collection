dfbc3aac5d3ae4cf8e31d5cf90272433fb748cde
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-06-19 21:37:16 +0100

    added background svg image

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './style.css';
 import Card from '../../components/card';
+import background from '../../assets/brain.svg';
 
 const Home = () => {
   return (
     <main id='home'>
+      <img className='background-image' src={background} alt="background"/>
       <form action='/'>
         <input type="text" placeholder='Search a project...'/>
       </form>
